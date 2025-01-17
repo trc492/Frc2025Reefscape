39ada725ed71d4e2fee7e6134152db3d981c0ed5
Titan Robotics Club <trc492@users.noreply.github.com>
2025-01-16 18:40:24 -0800

    Prep'd the code to be able to switch between last season or this season's robot base.

--- src/main/java/teamcode/subsystems/RobotBase.java
+++ src/main/java/teamcode/subsystems/RobotBase.java
@@ -31,8 +31,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
-import frclib.drivebase.FrcDifferentialDrive;
-import frclib.drivebase.FrcMecanumDrive;
 import frclib.drivebase.FrcRobotDrive;
 import frclib.drivebase.FrcSwerveDrive;
 import frclib.drivebase.FrcSwerveDrive.SteerEncoderType;
@@ -57,12 +55,10 @@ public class RobotBase
     {
         // This is useful for developing Vision code where all you need is a Robot Controller and camera.
         VisionOnly,
-        // Generic Differential Drive Base Robot
-        DifferentialRobot,
-        // Generic Mecanum Drive Base Robot
-        MecanumRobot,
-        // Generic Swerve Drive Base Robot
-        SwerveRobot
+        // Last season's robot.
+        CrescendoRobot,
+        // This season's robot.
+        ReefscapeRobot
     }   //enum RobotType
 
     /**
@@ -119,17 +115,14 @@ public class RobotBase
         }   //BackCamParams
     }   //class BackCamParams
 
-    /**
-     * This class contains the Swerve Robot Parameters.
-     */
-    public static class SwerveParams extends FrcSwerveDrive.SwerveInfo
+    public static class CrescendoRobotParams extends FrcSwerveDrive.SwerveInfo
     {
         public final double FALCON_MAX_RPM                      = 6380.0;
         public final double DRIVE_GEAR_RATIO                    = 6.75;
         public final double DRIVE_WHEEL_DIAMETER                = 3.9326556997620689090425924610785;    // inches
         public final double STEER_GEAR_RATIO                    = 15.43;
 
-        public SwerveParams()
+        public CrescendoRobotParams()
         {
             robotName = "SwerveRobot";
             // Robot Dimensions.
@@ -226,8 +219,118 @@ public class RobotBase
             // // ((theoretical max rpm * speed loss constant / gear ratio) / 60 sec/min) * 360 deg/rev
             // double steerMaxVelocity = (FALCON_MAX_RPM * 0.81 / STEER_GEAR_RATIO /60.0) * 360.0;
             // // kF set to Motion Magic recommendation.
-        }   //SwerveParams
-    }   //class SwerveParams
+        }   //CrescendoRobotParams
+    }   //class CrescendoRobotParams
+
+    /**
+     * This class contains the Swerve Robot Parameters.
+     */
+    public static class ReefscapeRobotParams extends FrcSwerveDrive.SwerveInfo
+    {
+        public final double FALCON_MAX_RPM                      = 6380.0;
+        public final double DRIVE_GEAR_RATIO                    = 6.75;
+        public final double DRIVE_WHEEL_DIAMETER                = 3.9326556997620689090425924610785;    // inches
+        public final double STEER_GEAR_RATIO                    = 15.43;
+
+        public ReefscapeRobotParams()
+        {
+            robotName = "SwerveRobot";
+            // Robot Dimensions.
+            robotLength = RobotParams.Robot.ROBOT_LENGTH;
+            robotWidth = RobotParams.Robot.ROBOT_WIDTH;
+            wheelBaseLength = 23.25;
+            wheelBaseWidth = 23.25;
+            // Gyro parameters.
+            imuName = "NavX";
+            imuType = FrcRobotDrive.ImuType.NavX;
+            imuPort = NavXComType.kMXP_SPI;
+            // Drive Motor parameters.
+            driveMotorType = MotorType.CanTalonFx;
+            driveMotorNames = new String[] {"lfDriveMotor", "rfDriveMotor", "lbDriveMotor", "rbDriveMotor"};
+            driveMotorIds = new int[] {
+                HwConfig.CANID_LFDRIVE_MOTOR, HwConfig.CANID_RFDRIVE_MOTOR,
+                HwConfig.CANID_LBDRIVE_MOTOR, HwConfig.CANID_RBDRIVE_MOTOR};
+            driveMotorInverted = new boolean[] {false, false, false, false};
+            odometryType = OdometryType.MotorOdometry;
+            // Absolute Odometry
+            absoluteOdometry = null;
+            // Drive Motor Odometry
+            xDrivePosScale = yDrivePosScale = DRIVE_WHEEL_DIAMETER * Math.PI / DRIVE_GEAR_RATIO;    // inch/rev
+            // Robot Drive Characteristics
+            robotMaxVelocity = 171.0;           // inch/sec
+            robotMaxAcceleration = 23000.0;     // inch/sec sq
+            robotMaxTurnRate = 1450.0;          // degree/sec
+            profiledMaxVelocity = 157.48;       // inch/sec
+            profiledMaxAcceleration = 10000.0;  // inch/sec sq
+            profiledMaxTurnRate = 180.0;        // degree/sec
+            // DriveBase PID Parameters
+            drivePidTolerance = 1.0;
+            turnPidTolerance = 2.0;
+            xDrivePidCoeffs = yDrivePidCoeffs = new PidCoefficients(0.017, 0.0, 0.0025, 0.0, 5.0);
+            xDrivePidPowerLimit = yDrivePidPowerLimit = 0.5;
+            xDriveMaxPidRampRate = yDriveMaxPidRampRate = 0.5;  // %power per sec
+            turnPidCoeffs = new PidCoefficients(0.0065, 0.0, 0.0004, 0.0, 10.0);
+            turnPidPowerLimit = 1.0;
+            turnMaxPidRampRate = 1.0;           // %power per sec
+            // PID Stall Detection
+            pidStallDetectionEnabled = true;
+            // PurePursuit Parameters
+            ppdFollowingDistance = 10.0;
+            velPidCoeffs = new PidCoefficients(0.0, 0.0, 0.0, 1.0 / robotMaxVelocity, 0.0);
+            // Front Camera
+            cam1 = new FrontCamParams();
+            // Back Camera
+            cam2 = new BackCamParams();
+            // Steer Encoder parameters.
+            steerEncoderType = SteerEncoderType.Canandmag;
+            steerEncoderNames = new String[] {"lfSteerEncoder", "rfSteerEncoder", "lbSteerEncoder", "rbSteerEncoder"};
+            steerEncoderIds = new int[] {
+                HwConfig.CANID_LFSTEER_ENCODER, HwConfig.CANID_RFSTEER_ENCODER,
+                HwConfig.CANID_LBSTEER_ENCODER, HwConfig.CANID_RBSTEER_ENCODER};
+            steerEncoderInverted = new boolean[] {false, false, false, false};
+            steerEncoderZeros = new double[] {0.0, 0.0, 0.0, 0.0};
+            steerZerosFilePath = RobotParams.Robot.STEER_ZERO_CAL_FILE;
+            // Steer Motor parameters.
+            steerMotorType = MotorType.CanTalonFx;
+            steerMotorNames = new String[] {"lfSteerMotor", "rfSteerMotor", "lbSteerMotor", "rbSteerMotor"};
+            steerMotorIds = new int[] {
+                HwConfig.CANID_LFSTEER_MOTOR, HwConfig.CANID_RFSTEER_MOTOR,
+                HwConfig.CANID_LBSTEER_MOTOR, HwConfig.CANID_RBSTEER_MOTOR};
+            steerMotorInverted = new boolean[] {false, false, false, false};
+            steerMotorPidCoeffs = new PidCoefficients(3.0, 0.0, 0.0, 0.0, 0.0);
+            steerMotorPidTolerance = 0.5; // in degrees
+            // Swerve Module parameters.
+            swerveModuleNames = new String[] {"lfWheel", "rfWheel", "lbWheel", "rbWheel"};
+            driveGearRatio = DRIVE_GEAR_RATIO;
+            //
+            // WPILib Parameters.
+            //
+            invertGyro = true;   // Always ensure Gyro is CCW+ CW-
+            // Drivetrain Constants
+            wheelBase = Units.inchesToMeters(wheelBaseLength);
+            trackWidth = Units.inchesToMeters(wheelBaseWidth);
+            wheelCircumference = Units.inchesToMeters(DRIVE_WHEEL_DIAMETER * Math.PI);
+            // Swerve Kinematics
+            // No need to ever change this unless you are not doing a traditional rectangular/square 4 module swerve
+            swerveKinematics = new SwerveDriveKinematics(
+                new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
+                new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+                new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+                new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+            // Meters per Second
+            maxSpeed = Units.inchesToMeters(profiledMaxVelocity);
+            // Radians per Second
+            maxAngularVelocity = Units.degreesToRadians(profiledMaxTurnRate);
+            driveKs = 0.32;
+            driveKv = 1.51;
+            driveKa = 0.27;
+
+            // double steerScaleDegPerRot = 360.0 / STEER_GEAR_RATIO;
+            // // ((theoretical max rpm * speed loss constant / gear ratio) / 60 sec/min) * 360 deg/rev
+            // double steerMaxVelocity = (FALCON_MAX_RPM * 0.81 / STEER_GEAR_RATIO /60.0) * 360.0;
+            // // kF set to Motion Magic recommendation.
+        }   //ReefscapeRobotParams
+    }   //class ReefscapeRobotParams
 
     /**
      * This class contains the Mecanum Robot Parameters.
@@ -377,19 +480,16 @@ public class RobotBase
                 robotDrive = null;
                 break;
 
-            case DifferentialRobot:
-                robotInfo = new DifferentialParams();
-                robotDrive = RobotParams.Preferences.useDriveBase? new FrcDifferentialDrive(robotInfo): null;
+            case CrescendoRobot:
+                robotInfo = new CrescendoRobotParams();
+                robotDrive = RobotParams.Preferences.useDriveBase?
+                    new FrcSwerveDrive((CrescendoRobotParams) robotInfo): null;
                 break;
 
-            case MecanumRobot:
-                robotInfo = new MecanumParams();
-                robotDrive = RobotParams.Preferences.useDriveBase? new FrcMecanumDrive(robotInfo): null;
-                break;
-
-            case SwerveRobot:
-                robotInfo = new SwerveParams();
-                robotDrive = RobotParams.Preferences.useDriveBase? new FrcSwerveDrive((SwerveParams) robotInfo): null;
+            case ReefscapeRobot:
+                robotInfo = new CrescendoRobotParams();
+                robotDrive = RobotParams.Preferences.useDriveBase?
+                    new FrcSwerveDrive((ReefscapeRobotParams) robotInfo): null;
                 break;
 
             default:
